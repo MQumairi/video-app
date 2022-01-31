142c8930036404aab63801cf3b1b73153d7e0843
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-01-31 19:47:41 +0000

    shuffle button to vid list

--- src/handlers/video_gui/GUI_List_Vids.ts
+++ src/handlers/video_gui/GUI_List_Vids.ts
@@ -6,7 +6,8 @@ const GUI_List_Vids = async (req: Request, res: Response): Promise<void> => {
   let directory_path = DirectoryManager.getDataPath() + "/" + dirname;
   let directory_manager = new DirectoryManager();
   let video_files = await directory_manager.listVideos(directory_path);
-  res.render("videos.ejs", { vids: video_files });
+  let random_vid = await directory_manager.randomVideo(directory_path);
+  res.render("videos.ejs", { vids: video_files, RANDOM_VID: random_vid, DIR_NAME: dirname });
 };
 
 export default GUI_List_Vids;

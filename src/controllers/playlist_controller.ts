37fac37bd75fdd63ddbbca50581104e3442df201
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-03-12 17:17:21 +0000

    Add multiple videos to a playlist

--- src/controllers/playlist_controller.ts
+++ src/controllers/playlist_controller.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import Add_Vid from "../handlers/playlists/Add_Vid";
+import Add_Vids from "../handlers/playlists/Add_Vids";
 import Create from "../handlers/playlists/Create";
 import Delete_Vid from "../handlers/playlists/Delete_Vid";
 
@@ -7,6 +8,10 @@ const playlist_controller = Router();
 
 playlist_controller.post("/add-video", async (req: Request, res: Response) => {
   await Add_Vid(req, res);
+});
+
+playlist_controller.post("/:dirname/add-videos", async (req: Request, res: Response) => {
+  await Add_Vids(req, res);
 });
 
 playlist_controller.post("/:id/delete-videos", async (req: Request, res: Response) => {

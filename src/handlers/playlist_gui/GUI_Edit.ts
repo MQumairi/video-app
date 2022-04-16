7e19b002c9245d943f97a4d0813bb7be3e94905a
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-04-16 14:39:22 +0100

    Videos listed on the playlist edit page are now sorted

--- src/handlers/playlist_gui/GUI_Edit.ts
+++ src/handlers/playlist_gui/GUI_Edit.ts
@@ -4,16 +4,29 @@ import { Playlist } from "../../models/playlist";
 import Shuffler from "../../models/shuffler";
 
 const GUI_edit = async (req: Request, res: Response): Promise<void> => {
-    const id = +req.params.id;
-    let playlist = await getRepository(Playlist).findOne(id);
-    if (playlist === undefined) {
-      playlist = new Playlist();
-      playlist.id = id;
-      playlist.name = "Unfound";
-      playlist.videos = [];
+  const id = +req.params.id;
+  let playlist = await getRepository(Playlist).findOne(id);
+  if (playlist === undefined) {
+    playlist = new Playlist();
+    playlist.id = id;
+    playlist.name = "Unfound";
+    playlist.videos = [];
+  }
+
+  playlist.videos.sort((vid_a, vid_b) => {
+    const name_a = vid_a.name.toUpperCase();
+    const name_b = vid_b.name.toUpperCase();
+    if (name_a < name_b) {
+      return -1;
     }
-    let random_vid_url = "/playlists/" + id + (await new Shuffler().playlist_shuffle(id));
-    res.render("playlists/playlist_edit.ejs", { PLAYLIST: playlist, RANDOM_VID: random_vid_url });
+    if (name_a > name_b) {
+      return 1;
+    }
+    return 0;
+  });
+
+  let random_vid_url = "/playlists/" + id + (await new Shuffler().playlist_shuffle(id));
+  res.render("playlists/playlist_edit.ejs", { PLAYLIST: playlist, RANDOM_VID: random_vid_url });
 };
 
 export default GUI_edit;

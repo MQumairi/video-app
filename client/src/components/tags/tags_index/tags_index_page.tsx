0735f1d0c2ca6b06d53f4c156d27905519e690f8
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-05-01 17:36:39 +0100

    tag index works

--- client/src/components/tags/tags_index/tags_index_page.tsx
+++ client/src/components/tags/tags_index/tags_index_page.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import { Tag } from "../../../api/agent";
+import ITag from "../../../models/tag";
+
 export const TagsIndexPage = () => {
+  const [tags, set_tags] = useState<ITag[]>([]);
+  const fetch_tags = async () => {
+    let received_tags = (await Tag.get()).data;
+    set_tags(received_tags);
+  };
+  useEffect(() => {
+    fetch_tags();
+  }, []);
   return (
     <div>
       Tags index page
+      {tags.map((tag) => {
+        return <div key={tag.name}>{tag.name}</div>;
+      })}
       <a href="/tags/new">Create</a>
     </div>
   );

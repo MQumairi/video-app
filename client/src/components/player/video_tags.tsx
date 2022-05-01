6a9434949658882e905ac398c25947862790ed09
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-05-01 23:51:53 +0100

    tags in video player styled

--- client/src/components/player/video_tags.tsx
+++ client/src/components/player/video_tags.tsx
@@ -1,11 +1,29 @@
 export const VideoTags = (props: any) => {
+  const video_tags = {
+    background: "#01141f",
+    display: "flex",
+    flexWrap: "wrap" as "wrap",
+    padding: "15px",
+  };
+  const tag_pill_style = {
+    background: "#064669",
+    borderRadius: "10px",
+    padding: "5px 10px 5px 10px",
+    width: "min-content",
+    margin: "10px",
+  };
   return (
-    <div>
-      <h1>
+    <div style={{ marginTop: "20px" }}>
+      <h2>Tags</h2>
+      <div style={video_tags}>
         {props.tags?.map((tag: any) => {
-          return <div>{tag.name}</div>;
+          return (
+            <a href={`/tags/${tag.id}`}>
+              <div style={tag_pill_style}>{tag.name}</div>
+            </a>
+          );
         })}
-      </h1>
+      </div>
     </div>
   );
 };

070b85b62def2f30c50f733ee6759cece2d596c6
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-05-01 02:04:01 +0100

    modified comment in directory controller.ts

--- server/src/controllers/directory_controller.ts
+++ server/src/controllers/directory_controller.ts
@@ -5,7 +5,7 @@ const directory_controller = Router();
 
 directory_controller.get("/:filepath", async (req: Request, res: Response) => {
   // Note: forward slashes need to be convered to %2F by the client
-  // Such that data/dir_1 => data%2Fdir_1
+  // Such that data/dir_1 => data%2Fdir_1, see directory_controller tests
   await Details(req, res);
 });
 

0cc93aef355eced1c1ec3744dde8190645d4ce3d
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-05-03 02:06:01 +0100

    tag ordered server side

--- server/src/handlers/tags/list.ts
+++ server/src/handlers/tags/list.ts
@@ -4,7 +4,7 @@ import { Tag } from "../../models/tag";
 
 const List = async (req: Request, res: Response): Promise<Tag[]> => {
   const tag_repo = getRepository(Tag);
-  const tag_list = await tag_repo.find();
+  const tag_list = await tag_repo.find({ order: { name: "ASC" } });
   res.json(tag_list);
   return tag_list;
 };

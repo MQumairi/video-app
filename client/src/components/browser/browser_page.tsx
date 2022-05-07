f4de689e52ad8bf237893b19ea7ab8f93fd9dd6d
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-05-07 19:15:37 +0100

    refactored browser page

--- client/src/components/browser/browser_page.tsx
+++ client/src/components/browser/browser_page.tsx
@@ -3,20 +3,13 @@ import { useEffect, useState } from "react";
 import { Directory } from "../../api/agent";
 import IDirectory from "../../models/directory";
 import { PathConverter } from "../../util/path_converter";
-import { DirectoryVideos } from "./directory_videos";
-import { SubDirectoryList } from "./sub_directory_list";
-import { HrefButton } from "../misc/href_button";
-import { ToggleButton } from "../misc/toggle_button";
-import { BrowserEditMode } from "./edit_videos/browser_edit_mode";
-import { ButtonGroup, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
+import { BrowserResults } from "./browser_results";
 
 const BrowserPage = () => {
   let dir_path = useParams().dir_path ?? "videos";
   const [directory, set_directory] = useState<IDirectory | null>(null);
   const [search_query, set_search_query] = useState<string>("");
-  const [edit_mode, set_edit_mode] = useState<boolean>(false);
-  const [tag_popover_visible, set_tag_popover_visible] = useState<boolean>(false);
-  const [check_all, set_check_all] = useState<boolean>(false);
 
   const fetch_directory = async (query: string) => {
     const api_query = PathConverter.to_query(query);
@@ -53,20 +46,11 @@ const BrowserPage = () => {
         onChange={handle_change}
         onKeyDown={handle_submit}
       />
-      <ButtonGroup>
-        <HrefButton href={(directory && PathConverter.to_query(directory.parent_path)) ?? "data"} textContent="Back" />
-        <ToggleButton toggle={edit_mode} set_toggle={set_edit_mode} trueText="Edit" />
-        {edit_mode && <ToggleButton toggle={check_all} set_toggle={set_check_all} falseText="Check All" trueText="Unlock Check" />}
-        {edit_mode && <ToggleButton toggle={tag_popover_visible} set_toggle={set_tag_popover_visible} trueText="Tag" />}
-      </ButtonGroup>
-      {!edit_mode && directory && <SubDirectoryList fetch_directory={fetch_directory} directory_paths={directory.directory_paths} />}
-      {!edit_mode && directory && <DirectoryVideos video_paths={directory.video_paths} />}
-      {edit_mode && directory && (
-        <BrowserEditMode
-          video_paths={directory.video_paths}
-          tag_popover_visible={tag_popover_visible}
-          set_tag_popover_visible={set_tag_popover_visible}
-          check_all={check_all}
+      {directory && (
+        <BrowserResults
+          back_url={PathConverter.to_query(directory.parent_path)}
+          directory_paths={directory.directory_paths ?? []}
+          videos={directory.video_paths ?? []}
         />
       )}
     </div>

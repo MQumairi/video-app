0fcc81f940d215199655f7d6e2cc2daa9e286e39
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2022-06-05 22:21:18 +0100

    mobx remove video works

--- client/src/components/player/player_page.tsx
+++ client/src/components/player/player_page.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { PathConverter } from "../../util/path_converter";
 import IVideoMeta from "../../models/video_meta";
 import { Playlist, Tag, Video } from "../../api/agent";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import HrefButton from "../misc/href_button";
 import ToggleButton from "../misc/toggle_button";
 import VideoPlayer from "./video_player";
@@ -10,22 +10,26 @@ import TagVideoPopover from "../popovers/tag_popover/tag_video_popover";
 import VideoTags from "./video_tags";
 import PlaylistVideoPopover from "../popovers/playlist_popover/playlists_video_popover";
 import { observer } from "mobx-react-lite";
+import SelectedVideosStore from "../../store/selected_videos_store";
+import TagPopoverButton from "../popovers/tag_popover/tag_popover_button";
+import PlaylistPopoverButton from "../popovers/playlist_popover/playlist_popover_button";
 
 const PlayerPage = () => {
   let vid_path = useParams().vid_path ?? "videos";
   let tag_id = useParams().tag_id;
   let playlist_id = useParams().playlist_id;
 
+  const selectedVideoStore = useContext(SelectedVideosStore);
+
   const [video_meta, set_video_meta] = useState<IVideoMeta | null>(null);
-  const [tag_toggled, set_tag_toggled] = useState<boolean>(false);
-  const [playlist_popover_visible, set_playlist_popover_visible] = useState<boolean>(false);
   const [random_vid_url, set_random_vid_url] = useState<string>("");
 
   const fetch_video_meta = async (query: string) => {
     const api_query = PathConverter.to_query(query);
     const received_video: IVideoMeta = (await Video.get(api_query)).data;
+    console.log("received:", received_video);
+    selectedVideoStore.set_single_selection(received_video);
     set_video_meta(received_video);
-    console.log(received_video);
   };
 
   const fetch_random_tag_video = async () => {
@@ -52,11 +56,11 @@ const PlayerPage = () => {
     <div>
       <h1>{video_meta?.name}</h1>
       <HrefButton href={get_parent_path()} textContent="Back" />
-      <ToggleButton toggle={tag_toggled} set_toggle={set_tag_toggled} trueText={"Tag"} />
-      <ToggleButton toggle={playlist_popover_visible} set_toggle={set_playlist_popover_visible} trueText="Playlist" />
+      <TagPopoverButton />
+      <PlaylistPopoverButton />
       {random_vid_url != "" && <HrefButton textContent="Random" href={random_vid_url} />}
-      {tag_toggled && <TagVideoPopover toggle={tag_toggled} set_toggle={set_tag_toggled} videos={[video_meta]} />}
-      {playlist_popover_visible && <PlaylistVideoPopover toggle={playlist_popover_visible} set_toggle={set_playlist_popover_visible} videos={[video_meta]} />}
+      {selectedVideoStore.tag_popover_visible && <TagVideoPopover />}
+      {selectedVideoStore.playlist_popover_visible && <PlaylistVideoPopover />}
       <VideoPlayer vid_path={vid_path} />
       <VideoTags tags={video_meta?.tags ?? []} />
     </div>

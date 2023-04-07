1e34441d228ae8166fbcc33001b8beba8f4035b3
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2023-04-07 01:40:17 +0100

    parse selected tags from search parameters on init

--- client/src/components/tags/util/searcher/tag_searcher.tsx
+++ client/src/components/tags/util/searcher/tag_searcher.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { TextField } from "@mui/material";
 import TagSearcherChip from "./tag_searcher_chip";
 import { Tag } from "../../../../api/agent";
+import { useSearchParams } from "react-router-dom";
 
 interface IProps {
   selected_tags: ITag[];
@@ -12,6 +13,7 @@ interface IProps {
 
 const TagSearcher: React.FC<IProps> = (props: IProps) => {
   const [tags, set_tags] = useState<ITag[]>([]);
+  const [search_params] = useSearchParams({});
 
   const remove_tag_object = (tag: ITag) => {
     const new_tags: ITag[] = [];
@@ -29,7 +31,26 @@ const TagSearcher: React.FC<IProps> = (props: IProps) => {
     let res = await Tag.get();
     if (res.status !== 200) return;
     console.log("received 200 response");
-    set_tags(res.data);
+    const fetched_tags: ITag[] = res.data;
+    set_tags(fetched_tags);
+    const selected_tag_ids = search_params.get("tags");
+    if (selected_tag_ids) {
+      const tags = tag_params_to_tags(fetched_tags, selected_tag_ids);
+      props.set_selected_tags(tags);
+    }
+  };
+
+  const tag_params_to_tags = (tags: ITag[], tags_params: string): ITag[] => {
+    const tag_ids = new Set(
+      tags_params.split("-").map((t) => {
+        return +t;
+      })
+    );
+    const tags_from_params: ITag[] = [];
+    for (const t of tags) {
+      if (tag_ids.has(t.id)) tags_from_params.push(t);
+    }
+    return tags_from_params;
   };
 
   useEffect(() => {

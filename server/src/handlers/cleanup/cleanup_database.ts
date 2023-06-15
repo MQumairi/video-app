a0a2302c7896373315f69e20ce4c2ff27754f810
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2023-06-15 11:43:53 +0100

    account for files with case insensitive filenames

--- server/src/handlers/cleanup/cleanup_database.ts
+++ server/src/handlers/cleanup/cleanup_database.ts
@@ -1,8 +1,24 @@
 import { Repository, getRepository } from "typeorm";
 import { Request, Response } from "express";
 import { VideoMeta } from "../../models/video_meta";
-import { existsSync } from "fs";
+import { readdirSync } from "fs";
 import { ImageGallery } from "../../models/image_gallery";
+
+function doesFileExist(filePath: string): boolean {
+  try {
+    const dirName = filePath.substring(0, filePath.lastIndexOf("/"));
+    const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+    const dirContents = readdirSync(dirName);
+
+    // Check if the file exists with case sensitivity
+    const fileExists = dirContents.some((file) => file === fileName);
+
+    return fileExists;
+  } catch (error) {
+    // Handle any errors, such as directory not found, etc.
+    return false;
+  }
+}
 
 const CleanupDatabase = async (req: Request, res: Response): Promise<void> => {
   console.log("cleaning database from missing videos");
@@ -11,11 +27,10 @@ const CleanupDatabase = async (req: Request, res: Response): Promise<void> => {
   let result = new Map<string, boolean>();
   const counter = { deleted: 0, kept: 0 };
   for (let v of videos) {
-    console.log(`checking videos ${v.id}`);
-    let path_exists = existsSync(v.path);
+    let path_exists = doesFileExist(v.path);
     result.set(v.name, path_exists);
     if (!path_exists) {
-      console.log(`deleting video ${v.id}`);
+      console.log(`deleting video ${v.name}`);
       await delete_video(v, video_repo);
       counter.deleted += 1;
     } else {

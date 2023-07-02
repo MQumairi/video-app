a0492c1fdcf304270dedb997ec71b260d993c039
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2023-07-02 22:30:28 +0100

    create gallery if missing

--- client/src/store/video_store.ts
+++ client/src/store/video_store.ts
@@ -58,11 +58,8 @@ class VideoStore {
     this.selected_video_tags = res.data;
   };
 
-  @action lookup_selected_video_gallery = async () => {
-    if (!this.selected_video) return;
-    const res = await Video.gallery(this.selected_video);
-    if (res.status !== 200) return;
-    this.selected_video_gallery = res.data;
+  @action set_selected_video_gallery = async (gallery: IImageGallery | undefined) => {
+    if (gallery) this.selected_video_gallery = gallery;
   };
 
   @action lookup_selected_video_scripts = async () => {

7dfe5a46ca243911d41ca18d26d65258708218fe
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2023-07-05 22:39:08 +0100

    introduce file_operations class

---
+++ server/src/lib/file_system/file_operations.ts
@@ -0,0 +1,23 @@
+import { rename } from "fs/promises";
+import { mkdirSync, existsSync } from "fs";
+import { dirname } from "path";
+
+export class FileOperation {
+  static async move(src_path: string, dest_path: string): Promise<boolean> {
+    // Create directory of dest_path if it doesn't exist
+    FileOperation.create_dir(dirname(dest_path));
+    // Move file to directory of dest_path
+    try {
+      await rename(src_path, dest_path);
+    } catch (err) {
+      console.log("err:", err);
+    }
+    return existsSync(dest_path);
+  }
+
+  static async create_dir(dir_path: string) {
+    if (!existsSync(dir_path)) {
+      mkdirSync(dir_path, { recursive: true });
+    }
+  }
+}
--- server/src/lib/file_trasher.ts
+++ server/src/lib/file_trasher.ts
@@ -1,29 +1,12 @@
-import { mkdirSync, existsSync } from "fs";
-import { rename } from "fs/promises";
-import path from "path";
+import { join, dirname, basename } from "path";
+import { FileOperation } from "./file_system/file_operations";
 
 export class FileTrasher {
   static TRASH_PATH = "trash/";
 
   static async trash(file_path: string): Promise<boolean> {
-    const trash_dir_path = this.find_or_create_trash_dir(file_path);
-    const file_name = path.basename(file_path);
-    const destination_path = path.join(trash_dir_path, file_name);
-    let res = false;
-    try {
-      await rename(file_path, destination_path);
-    } catch (err) {
-      console.log("err:", err);
-    }
-    return existsSync(destination_path);
-  }
-
-  private static find_or_create_trash_dir(file_path: string): string {
-    const trash_dir = path.join(this.TRASH_PATH, path.dirname(file_path));
-    if (!existsSync(trash_dir)) {
-      console.log("making trash");
-      mkdirSync(trash_dir, { recursive: true });
-    }
-    return trash_dir;
+    const trash_dir = join(this.TRASH_PATH, dirname(file_path));
+    const destination_path = join(trash_dir, basename(file_path));
+    return FileOperation.move(file_path, destination_path);
   }
 }

5c30d24c1a72dd608c6688c33ede4face6b15427
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2023-10-03 19:19:48 +0100

    can execute global scripts from the client side

--- client/src/components/file_scripts/details/global_script_details.tsx
+++ client/src/components/file_scripts/details/global_script_details.tsx
@@ -1,15 +1,28 @@
 import { observer } from "mobx-react-lite";
 import IFileScript from "../../../models/file_script";
-import PublicIcon from "@mui/icons-material/Public";
+import { Button } from "@mui/material";
+import { FileScripts } from "../../../api/agent";
 
 interface IProps {
   script: IFileScript;
 }
 
-const GlobalScriptDetails = (props: IProps) => (
-  <div>
-    <PublicIcon fontSize="large" />
-  </div>
-);
+const GlobalScriptDetails = (props: IProps) => {
+  const on_execute = async () => {
+    console.log("executing", props.script.name);
+    await FileScripts.execute_global_script(props.script, "");
+  };
+
+  return (
+    <div>
+      <div style={{ marginTop: "20px" }}>
+        <p>Execute Global script '{props.script.name}'</p>
+        <Button variant="contained" onClick={on_execute}>
+          Execute
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default observer(GlobalScriptDetails);

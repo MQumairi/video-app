bf43dc9245db6b30bf9b3975af189d24f2bbf792
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2024-01-11 14:25:53 +0300

    a way to export tags

--- server/src/handlers/cleanup/cleanup_exported_data.ts
+++ server/src/handlers/cleanup/cleanup_exported_data.ts
@@ -3,6 +3,8 @@ import { DataExporter } from "../../lib/data_exporter";
 
 const CleanupExportedData = async (req: Request, res: Response): Promise<void> => {
   console.log("exporting data now...");
+  console.log("exporting tags...");
+  await DataExporter.export_tags();
   console.log("cleaning up videos...");
   await DataExporter.export_videos();
   res.status(200).send({ message: "ok" });
--- server/src/lib/data_exporter.ts
+++ server/src/lib/data_exporter.ts
@@ -1,5 +1,5 @@
-// tags
-// images & galleries
+// tags ****
+// galleries
 // videos ****
 // persistent_queries
 // playlists
@@ -18,9 +18,11 @@ interface DataObject {
 export class DataExporter {
   static export_dir_name = "exported_data";
   static video_file_path = join(DataExporter.export_dir_name, "exported_videos.json");
+  static tag_file_path = join(DataExporter.export_dir_name, "exported_tags.json");
 
   static async export_videos() {
-    await FileOperation.create_file(DataExporter.video_file_path, "");
+    const file_path = DataExporter.video_file_path;
+    await FileOperation.create_file(file_path, "");
     const video_meta_repo = getRepository(VideoMeta);
     const video_query = video_meta_repo
       .createQueryBuilder("video")
@@ -35,9 +37,27 @@ export class DataExporter {
     const vidoes = await video_query.getMany();
     for (const v of vidoes) {
       const obj = DataExporter.videoToJson(v);
-      DataExporter.appendObjectToFile(DataExporter.video_file_path, obj);
+      DataExporter.appendObjectToFile(file_path, obj);
     }
-    DataExporter.cleanupFile(DataExporter.video_file_path);
+    DataExporter.cleanupFile(file_path);
+  }
+
+  static async export_tags() {
+    const file_path = DataExporter.tag_file_path;
+    await FileOperation.create_file(file_path, "");
+    const tag_repo = getRepository(Tag);
+    const tags_query = tag_repo
+      .createQueryBuilder("tag")
+      .addGroupBy("tag.id")
+      .leftJoinAndSelect("tag.child_tags", "child_tag")
+      .addGroupBy("child_tag.id")
+      .orderBy("tag.id");
+    const tags = await tags_query.getMany();
+    for (const t of tags) {
+      const obj = DataExporter.tagToJson(t);
+      DataExporter.appendObjectToFile(file_path, obj);
+    }
+    DataExporter.cleanupFile(file_path);
   }
 
   private static videoToJson = (video: VideoMeta): DataObject => {
@@ -57,6 +77,20 @@ export class DataExporter {
       gallery: video.gallery ? video.gallery.path : null,
     };
     return video_object;
+  };
+
+  private static tagToJson = (tag: Tag): DataObject => {
+    console.log("tag:", tag.name);
+    const tag_object = {
+      name: tag.name,
+      is_character: tag.is_character,
+      is_series: tag.is_series,
+      is_studio: tag.is_studio,
+      default_excluded: tag.default_excluded,
+      default_hidden: tag.default_hidden,
+      child_tags: Tag.get_names(tag.child_tags),
+    };
+    return tag_object;
   };
 
   private static appendObjectToFile = (filePath: string, obj: DataObject) => {

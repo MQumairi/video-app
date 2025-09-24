9bd9bd52462d63455912e73f7192ed7b3282c472
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2025-09-24 18:21:33 +0100

    print when global script executed

--- server/src/handlers/file_scripts/execute_global.ts
+++ server/src/handlers/file_scripts/execute_global.ts
@@ -3,6 +3,7 @@ import { getRepository } from "typeorm";
 import { FileScript } from "../../models/file_script";
 
 const ExecuteGlobal = async (req: Request, res: Response): Promise<FileScript | undefined> => {
+  console.log("executing global script");
   const id = +req.params.id;
   const file_script_repo = getRepository(FileScript);
   const script = await file_script_repo.findOne(id);
@@ -10,6 +11,7 @@ const ExecuteGlobal = async (req: Request, res: Response): Promise<FileScript | 
     res.status(404).send({ message: "file script not found" });
     return;
   }
+  console.log("executing global script: ", script.name);
   const args = req.body.args ?? "";
   await FileScript.execute_script(script, args);
   res.status(200).send(script);
